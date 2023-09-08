a0795d403cf6f30540b555ae56a907f253830072
NZBOLZ <142586331+NZBOLZ@users.noreply.github.com>
2023-09-08 13:29:52 +0200

    Update secondtry.py

--- secondtry.py
+++ secondtry.py
@@ -2,5 +2,5 @@ import streamlit
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥣 🥗 🐔 🥑🍞Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
