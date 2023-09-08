d921ef26da3dd97d35878e61ebbad974c1c1fce1
NZBOLZ <142586331+NZBOLZ@users.noreply.github.com>
2023-09-08 13:29:28 +0200

    Update secondtry.py

--- secondtry.py
+++ secondtry.py
@@ -1,4 +1,6 @@
 import streamlit
 
-streamlit.title('App yea yeah')
-streamlit.header('headerrryeah')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
